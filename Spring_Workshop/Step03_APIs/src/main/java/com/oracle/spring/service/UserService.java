cf12e23ba4487fec06f5ee14fc124153b1ed66d8
ccrock <cam.crockett.12@gmail.com>
2015-12-29 21:58:54 -0700

    Fixing all the Encryption stuff. Looks Good, should finish off tomorrow.

--- Spring_Workshop/Step03_APIs/src/main/java/com/oracle/spring/service/UserService.java
+++ Spring_Workshop/Step03_APIs/src/main/java/com/oracle/spring/service/UserService.java
@@ -4,57 +4,64 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.log4j.Logger;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.oracle.spring.dao.UserDao;
 import com.oracle.spring.util.Gravatar;
-import com.springcryptoutils.core.cipher.symmetric.Cipherer;
 
 @Service
 @Transactional
 public class UserService {
 
+	private static final Logger logger = Logger.getLogger(UserService.class);
+
 	@Resource
 	private UserDao dao;
 
-	private static final byte[] iv = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-	private static final byte[] key = "superSecretKey".getBytes();
-
-	@Resource
-	@Qualifier("encrypter")
-	private Cipherer encrypter;
-
-	@Resource
-	@Qualifier("decrypter")
-	private Cipherer decrypter;
+	public static final String KEY = "SuperSecretKey";
 
 	public Boolean changePassword(String email, String oldpw, String newpw) {
-		String oldHash = String.valueOf(encrypter.encrypt(key, iv, oldpw.getBytes()));
-		String newHash = String.valueOf(encrypter.encrypt(key, iv, newpw.getBytes()));
+		StandardPasswordEncoder encoder = new StandardPasswordEncoder(KEY);
+		String newHash = encoder.encode(newpw);
+
 		Map<String, Object> user = dao.getUserByEmail(email);
-		if (oldHash.equals(user.get("USER_PASSWORD"))) {
-			dao.update(email, newHash);
-			return true;
+		try {
+			if (encoder.matches(oldpw, (String) user.get("user_password"))) {
+				dao.update(email, newHash);
+				return true;
+			}
+		} catch (Exception e) {
+			logger.error("Error matching", e);
 		}
 		return false;
 	}
 
 	public Map<String, Object> createUser(String username, String email, String password) {
-		String hash = String.valueOf(encrypter.encrypt(key, iv, password.getBytes()));
+		StandardPasswordEncoder encoder = new StandardPasswordEncoder(KEY);
+		String hash = encoder.encode(password);
 		String gravatar = Gravatar.url(email);
 		dao.createUser(username, email, hash, gravatar);
 		return dao.getUserByEmail(email);
 	}
 
 	public Map<String, Object> login(String email, String password) {
-		byte[] hash = encrypter.encrypt(key, iv, password.getBytes());
+		StandardPasswordEncoder encoder = new StandardPasswordEncoder(KEY);
 		Map<String, Object> user = dao.getUserByEmail(email);
-		if (user != null && hash.equals(user.get("USER_PASSWORD"))) {
-			return user;
+		try {
+			if (user != null && encoder.matches(password, (String) user.get("user_password"))) {
+				return user;
+			}
+		} catch (Exception e) {
+			logger.error("Error matching", e);
 		}
 		return null;
+	}
+
+	public void setDao(UserDao dao) {
+		this.dao = dao;
 	}
 
 }
